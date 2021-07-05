17daf6b73f680f2d388fd3e04682ece9bef1c060
yxj <17804331763@163.com>
2021-07-05 09:06:03 +0800

    add JDBC

---
+++ JavaWeb/Web-10-JDBC/src/main/java/utils/MyBatisUtils.java
@@ -0,0 +1,34 @@
+package utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author LYJ
+ * @create 2021-07-04 22:09
+ */
+public class MyBatisUtils {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSession getSqlSession(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        return sqlSession;
+    }
+
+}
