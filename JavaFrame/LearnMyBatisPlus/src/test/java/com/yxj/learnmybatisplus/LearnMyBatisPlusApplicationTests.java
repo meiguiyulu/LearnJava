d7855f6f4ec6a4a14f8038e114819cf173d6ee47
玫瑰与鹿 <33453157+meiguiyulu@users.noreply.github.com>
2021-11-23 22:05:38 +0800

    Insert and Update

--- JavaFrame/LearnMyBatisPlus/src/test/java/com/yxj/learnmybatisplus/LearnMyBatisPlusApplicationTests.java
+++ JavaFrame/LearnMyBatisPlus/src/test/java/com/yxj/learnmybatisplus/LearnMyBatisPlusApplicationTests.java
@@ -21,4 +21,32 @@ class LearnMyBatisPlusApplicationTests {
         userList.forEach(System.out::println);
     }
 
+    @Test
+    public void testInsert() {
+        User user = new User();
+        user.setName("云杰");
+        user.setAge(22);
+        user.setEmail("1609226090@qq.com");
+        /**
+         * 运作下面结果发现：
+         *  1、id会帮我们自动生成
+         *  2、insert表示受影响的行数
+         */
+        int insert = userMapper.insert(user);
+        System.out.println(insert);
+        System.out.println(user);
+    }
+
+    // 测试更新
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        user.setId(1_463_109_181_069_959_169L);
+        user.setName("小杰");
+        user.setAge(24);
+        user.setEmail("1609226090@qq.com");
+        int update = userMapper.updateById(user);
+        System.out.println(update);
+    }
+
 }
