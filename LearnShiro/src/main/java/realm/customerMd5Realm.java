79e8201eb468bdc6629a7e5882fa36de6cb6b023
玫瑰与鹿 <33453157+meiguiyulu@users.noreply.github.com>
2022-03-18 13:32:31 +0800

    Shiro 认证+授权

--- LearnShiro/src/main/java/realm/customerMd5Realm.java
+++ LearnShiro/src/main/java/realm/customerMd5Realm.java
@@ -5,6 +5,7 @@ import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
@@ -13,9 +14,22 @@ import org.apache.shiro.util.ByteSource;
  * 使用自定义realm 加入md5 + salt +hash
  */
 public class customerMd5Realm extends AuthorizingRealm {
+    // 授权
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        return null;
+        String primaryPrincipal = (String) principalCollection.getPrimaryPrincipal();
+        System.out.println("身份信息: " + primaryPrincipal); //用户名
+
+        //根据身份信息 用户名 获取当前用户的角色信息，以及权限信息
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        //假设 admin,user 是从数据库查到的 角色信息
+        simpleAuthorizationInfo.addRole("admin");
+        simpleAuthorizationInfo.addRole("user");
+        //假设 ... 是从数据库查到的 权限信息赋值给权限对象
+        simpleAuthorizationInfo.addStringPermission("user:*:01");
+        simpleAuthorizationInfo.addStringPermission("prodect:*");//第三个参数为*省略
+
+        return simpleAuthorizationInfo;
     }
 
     @Override
@@ -23,8 +37,8 @@ public class customerMd5Realm extends AuthorizingRealm {
         // 获取用户信息
         String principal = (String) authenticationToken.getPrincipal();
         //假设这是从数据库查询到的信息
-        String username="zhangsan";
-        String password="7268f6d32ec8d6f4c305ae92395b00e8";//加密后
+        String username = "zhangsan";
+        String password = "7268f6d32ec8d6f4c305ae92395b00e8";//加密后
 
         //根据用户名查询数据库
         if (username.equals(principal)) {
--- LearnShiro/src/main/java/testCustomerMd5Realm.java
+++ LearnShiro/src/main/java/testCustomerMd5Realm.java
@@ -7,6 +7,8 @@ import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.subject.Subject;
 import realm.customerMd5Realm;
 
+import java.util.Arrays;
+
 public class testCustomerMd5Realm {
     public static void main(String[] args) {
         //1.创建安全管理器
@@ -42,6 +44,39 @@ public class testCustomerMd5Realm {
         }catch (IncorrectCredentialsException e){
             e.printStackTrace();
             System.out.println("密码错误");
+        }
+
+        //授权
+        if (subject.isAuthenticated()){
+            //基于角色权限控制
+            System.out.println(subject.hasRole("admin"));
+            //基于多角色的权限控制
+            System.out.println(subject.hasAllRoles(Arrays.asList("admin", "user")));//true
+            System.out.println(subject.hasAllRoles(Arrays.asList("admin", "manager")));//false
+            //是否具有其中一个角色
+            boolean[] booleans = subject.hasRoles(Arrays.asList("admin", "user", "manager"));
+            for (boolean aBoolean : booleans) {
+                System.out.println(aBoolean);
+            }
+
+            System.out.println("====这是一个分隔符====");
+
+            //基于权限字符串的访问控制  资源标识符：操作：资源类型
+            //用户具有的权限 user:*:01  prodect:*
+            System.out.println("权限:"+subject.isPermitted("user:update:01"));
+            System.out.println("权限:"+subject.isPermitted("prodect:update:02"));
+
+            //分别具有哪些权限
+            boolean[] permitted = subject.isPermitted("user:*:01", "user:update:02");
+            for (boolean b : permitted) {
+                System.out.println(b);
+            }
+
+            //同时具有哪些权限
+            boolean permittedAll = subject.isPermittedAll("prodect:*:01", "prodect:update:03");
+            System.out.println(permittedAll);
+
+
         }
     }
 }
