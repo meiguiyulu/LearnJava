7e2e97496532fc260565d453590ee7f5661bc091
玫瑰与鹿 <33453157+meiguiyulu@users.noreply.github.com>
2022-04-10 20:25:35 +0800

    获取登录用户数据

--- LearnSpringSecurity/demo02/src/main/java/com/lyj/demo02/controller/HelloController.java
+++ LearnSpringSecurity/demo02/src/main/java/com/lyj/demo02/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.lyj.demo02.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,10 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
         System.out.println("Hello Spring Security");
+        // 1. 获取认证信息
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("身份信息: " + authentication.getPrincipal());
+        System.out.println("权限信息: " + authentication.getAuthorities());
         return "Hello Spring Security";
     }
 }
