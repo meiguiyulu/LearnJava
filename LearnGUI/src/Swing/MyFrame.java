68c60ec892ed851684a53e85fa41570c21330b83
玫瑰与鹿 <33453157+meiguiyulu@users.noreply.github.com>
2022-05-24 19:37:48 +0800

    显示当前时间

--- LearnGUI/src/Swing/MyFrame.java
+++ LearnGUI/src/Swing/MyFrame.java
@@ -1,6 +1,11 @@
 package Swing;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @Author LiuYunJie
@@ -8,7 +13,9 @@ import javax.swing.*;
  **/
 public class MyFrame extends JFrame {
 
-    public MyFrame(String title){
+    JLabel timeLabel = new JLabel("点击按钮，显示时间");
+
+    public MyFrame(String title) {
         super(title);
         /*
         设置容器
@@ -21,8 +28,25 @@ public class MyFrame extends JFrame {
          */
         JLabel label = new JLabel("JLabel标签");
         rootPanel.add(label);
+        rootPanel.add(timeLabel);
 
         JButton button = new JButton("测试");
         rootPanel.add(button);
+        /* 添加事件处理 */
+        ActionListener actionListener = new MyActionListener();
+        button.addActionListener(actionListener);
     }
+
+    private class MyActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.print("事件处理初体验:\t");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+            String format = dateFormat.format(new Date());
+            System.out.println(format);
+
+            timeLabel.setText(format);
+        }
+    }
+
 }
